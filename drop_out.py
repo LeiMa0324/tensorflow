b8eeaf34839910380ec480738e9eec5da82711cc
malei <gorillazhip@126.com>
2018-08-30 22:06:23 +0800

    可视化

--- drop_out.py
+++ drop_out.py
@@ -16,13 +16,14 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3)
 def add_layer(inputs,in_size,out_size,layer_name,avtivation_function=None,):
     Weights = tf.Variable(tf.random_normal([in_size,out_size]))
     biases = tf.Variable(tf.zeros([1,out_size])+0.1)
-    Wx_plus_b = tf.matmul(Weights,inputs)+biases
+    Wx_plus_b = tf.matmul(inputs,Weights)+biases
 
     if avtivation_function is None:
-        output = Wx_plus_b
+        outputs = Wx_plus_b
     else:
-        output = avtivation_function(Wx_plus_b)
-    return output
+        outputs = avtivation_function(Wx_plus_b)
+    tf.summary.histogram(layer_name+'/outputs',outputs)
+    return outputs
 
 '''
 placeholder
@@ -34,19 +35,37 @@ ys = tf.placeholder(tf.float32,[None,10])
 add layers
 '''
 # 隐藏层,100个神经元
-l1 = add_layer(xs,64,100,'l1',activation_function= tf.nn.tanh)
+l1 = add_layer(xs,64,100,'l1',avtivation_function= tf.nn.tanh)
 #输出为10位的概率
-prediction = add_layer(l1,100,10,'l2',activation_function=tf.nn.softmax)
+prediction = add_layer(l1,100,10,'l2',avtivation_function=tf.nn.softmax)
 
 '''
 loss & training step
 '''
 #交叉熵
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction)),reduction_indices=1)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
 tf.summary.scalar("loss",cross_entropy)
 train_step = tf.train.GradientDescentOptimizer(0.6).minimize(cross_entropy)
 
 '''
+开始训练
 '''
 sess = tf.Session()
 merged = tf.summary.merge_all()
+
+'''
+summary writer
+'''
+train_writer = tf.summary.FileWriter("logs/train",sess.graph)
+test_writer = tf.summary.FileWriter("logs/test",sess.graph)
+
+sess.run(tf.initialize_all_variables())
+
+for i in range(500):
+    sess.run(train_step,feed_dict={xs:X_train,ys:y_train})
+    if i%50 ==0:
+        train_result = sess.run(merged,feed_dict={xs:X_train,ys:y_train})
+        test_result = sess.run(merged,feed_dict={xs:X_test,ys:y_test})
+
+        train_writer.add_summary(train_result,i)
+        test_writer.add_summary(test_result,i)
