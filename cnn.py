798d49f85955a5e09c5adbd02ddbf99ef74f6757
malei <gorillazhip@126.com>
2018-09-03 12:01:10 +0800

    可视化

--- cnn.py
+++ cnn.py
@@ -18,22 +18,39 @@ def compute_accuracy(v_xs,v_ys):
 输入shape，得到weight和bias的初始值
 '''
 def weight_variable(shape):
-    pass
+    #产生随机变量
+    initial = tf.truncated_normal(shape,stddev=0.1)
+    return tf.Variable(initial)
 
 def bias_variable(shape):
-    pass
+    # bias初始值为0.1
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
 
+'''
+定义卷积网络神经层
+输入图片信息和Weights
+'''
 def conv2d(x,W):
-    pass
+    # stride = [1,x_movement,y_movement,1]
+    # PADDING ='VALID' OR 'SAME'
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding= 'SAME')
 
+'''
+定义pooling层
+'''
 def max_pool_2x2(x):
-    pass
+    # stride = [1,x_movement,y_movement,1]
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
 #define placeholder
 xs = tf.placeholder(tf.float32,[None,784])#28*28
 ys = tf.placeholder(tf.float32,[None,10])
-
 keep_prob = tf.placeholder(tf.float32)
+
+# -1为不管有多少个样本保持不变，样本内为28*28,1为channel
+# 将xs的数据reshape为conv2d接受的input=[batch,in_heigh,in_weight,in_channels]的形式
+x_image = tf.reshape(xs,[-1,28,28,1])
 
 '''
 conv1 layer
@@ -61,11 +78,13 @@ sess = tf.Session()
 
 '''
 important step
+重要步骤
 '''
 sess.run(tf.initialize_all_variables)
 
 '''
 start training
+开始训练
 '''
 for i in range(1000):
      batch_xs,batch_ys=mnist.train.next_batch(100)
